d60c98e34bb85bb8c0d065cea67dad72c5ab631a
uedame <uedame0403@gmail.com>
2023-03-14 08:32:59 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+const [showText,setShowText] =useState(true);
+
+  const onClickShow = () => {
+    setShowText(!showText)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click Counter
+      </button>
+      <br />
+      <br />
+      <button onClick={onClickShow}>Text on/off</button>
+      { showText && <p>BB WORLD</p> }
+    </>
   );
 }
 
